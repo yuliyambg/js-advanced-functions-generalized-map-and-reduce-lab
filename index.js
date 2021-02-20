8405c78918277915cb73bf568c0df19f86bbeceb
yuliyambg <yuliyambg@gmail.com>
2021-02-20 01:14:38 -0500

    commit tree 1d6b9688f1f55f29595962664d7ffbe2a2dcd30c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+function map(arr, fn) {
+    return arr.map (element => fn(element))
+}
+
+function reduce(arr, fn, start) {
+    let result = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (; i < arr.length; i++) {
+        result = fn(arr[i], result);
+    }
+
+    return result;
+}
+
+// function reduce(arr, reducer, initialValue) {
+//  let total = (!!initialValue) ? initialValue : 0
+//
+//     for (const val of arr) {
+//         total = reducer(val, total);
+//     }
+//     return total;
+// }
